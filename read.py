8c19f7c345551a632a62a6574eaf940fa0b2e7c6
Johannes Hölzl <johannes.hoelzl@gmx.de>
2019-03-06 18:48:49 +0100

    built up search graph

--- read.py
+++ read.py
@@ -2,81 +2,147 @@
 import re
 import collections
 
-inst = re.compile(r"\[class_instances\]\ \((\d+)\)\ \?x_(\d*)\ ([^:]*):\ (.*?):=\ (.*)", re.DOTALL | re.VERBOSE)
+log_regex = re.compile(r"""
+  (\[class_instances\]\ \((?P<depth>\d+)\)
+    \ \?x_(?P<mvar>\d+)\ (?P<locals>[^:]*):\ (?P<type>.*)\ :=\ (?P<val>.*)) |
+  (\[type_context.tmp_vars\]\ (?P<cmd1>(assign)|(unassign))\ \?x_(?P<mvar2>\d+)\ :=\ (?P<val2>.*)) |
+  (\[type_context.tmp_vars\]\ (?P<cmd2>(push_scope)|(pop_scope)),\ trail_sz:\ (?P<sz>\d+)) |
+  (?P<fail>failed\ is_def_eq)""",
+  re.DOTALL | re.VERBOSE)
 
-class Inst:
-  def __init__(self, depth, varname, params, type, value):
-    self.parent = None
-    self.depth = int(depth)
-    self.varname = varname
-    self.params = params.split()
-    self.type = type
-    self.value = value
+class Parser:
+  def parse(self, f):
+    p = ""
+    for l in f.readlines():
+      if l:
+        if l.startswith("  "):
+          p += " " + l.strip()
+        else:
+          if p != '':
+            self.parse_paragraph(p)
+          p = l.strip()
+      else:
+        p = l.strip()
+    if p != '':
+      self.parse_paragraph(p)
+    self.finished()
 
-    self.failed = False
-    self.nodes = collections.OrderedDict()
+  def parse_paragraph(self, p):
+    m = log_regex.fullmatch(p)
+    if not m:
+      print("Unkown log message:", repr(p))
+      return
 
-  def set_parent(self, parent):
+    d = m.groupdict()
+    if d['depth']:
+      self.apply_instance(int(d["depth"]), int(d["mvar"]), d["locals"].split(),
+        d["type"].strip(), d["val"].strip())
+    elif d["cmd1"] == "assign":
+      self.assign(int(d["mvar2"]), d["val2"].strip())
+    elif d["cmd1"] == "unassign":
+      self.unassign(int(d["mvar2"]), d["val2"].strip())
+    elif d["cmd2"] == "push_scope":
+      self.push_scope(int(d["sz"]))
+    elif d["cmd2"] == "pop_scope":
+      self.pop_scope(int(d["sz"]))
+    elif d["fail"]:
+      self.apply_failed()
+
+  def finished(self):
+    pass
+
+  def apply_instance(self, depth, mvar, locals, type, val):
+    pass
+
+  def apply_failed(self):
+    pass
+
+  def assign(self, mvar, val):
+    pass
+
+  def unassign(self, mvar, val):
+    pass
+
+  def push_scope(self, sz):
+    pass
+
+  def pop_scope(self, sz):
+    pass
+
+class Instance:
+  """ Instance assignment (with successfull def_eq) """
+  def __init__(self, scope, cnt, d, m, l, t, v):
+    self.scope = scope
+    self.cnt = cnt
+    self.depth = d
+    self.mvar = m
+    self.locals = l
+    self.type = t
+    self.val = v
+
+  def print(self):
+    print("% 5i  (% 2i)  ?x_%i : %s := %s" % (self.cnt, self.depth, self.mvar, self.type, self.val))
+
+class Scope:
+  """ Search scope per instance """
+  def __init__(self, parent):
     self.parent = parent
     if self.parent:
-      self.parent.nodes.setdefault(self.varname, []).append(self)
-
-  def is_success(self):
-    if self.failed: return False
-
-    for (var, lst) in self.nodes.items():
-      for i in lst:
-        if i.is_success(): break
-      else:
-        return False
-
-    return True
-
-  def print_tree(self):
-    if self.failed: return
-
-    print("BEGIN (%i) ?x_%s : %s := %s".format(self.depth, self.varname, self.type, self.value))
-    for (var, lst) in self.nodes.items():
-      for i in lst:
-        i.print_tree()
-    print("END ?x_%s".format(self.varname))
-
-def paragraph(lines):
-  paragraph = ""
-  for l in lines:
-    if l:
-      if l.startswith("  "):
-        paragraph += " " + l.strip()
-      else:
-        yield paragraph
-        paragraph = l.strip()
+      assert(parent.applies)
+      self.instance = parent.applies[-1]
+      self.parent.subscopes.append(self)
     else:
-      paragraph = l.strip()
-  yield paragraph
+      self.instance = None
+    self.applies = []
+    self.subscopes = []
+    self.mvars = collections.OrderedDict()
 
-def parser(paragraphs):
-  current = None
-  for p in paragraphs:
-    m = inst.fullmatch(p)
-    if m:
-      new = Inst(m.group(1), m.group(2), m.group(3), m.group(4), m.group(5))
-      while current and current.depth >= new.depth:
-        current = current.parent
-      new.set_parent(current)
-      current = new
+  def print(self):
+    if self.instance:
+      self.instance.print()
+    print("     subscopes ", len(self.subscopes))
 
-    elif p == "failed is_def_eq":
-      if current:
-        current.failed = True
-        current = current.parent
-    elif p == "":
+class ContextParser(Parser):
+  def __init__(self):
+    self.apply_cnt = 0
+    self.scope = Scope(None)
+
+  def push_scope(self, sz):
+    self.scope = Scope(self.scope)
+
+  def pop_scope(self, sz):
+    self.scope = self.scope.parent
+
+  def assign(self, mvar, val):
+    self.scope.mvars[mvar] = val
+
+  def unassign(self, mvar, val):
+    if mvar in self.scope.mvars:
+      del self.scope.mvars[mvar]
+    else:
+      # print("cannot unnassign %i" % mvar)
       pass
-    else:
-      raise Exception("unknown", p)
 
-  return current
+  def apply_instance(self, d, m, l, t, v):
+    self.apply_cnt += 1
+    self.scope.applies.append(Instance(self.scope, self.apply_cnt, d, m, l, t, v))
+
+  def apply_failed(self):
+    self.scope.applies.pop()
+    self.apply_cnt -= 1
+
+  def finished(self):
+    scope = self.scope
+    scopes = []
+    while scope and scope.parent:
+      scopes.insert(0, scope)
+      scope = scope.parent
+
+    print ("scopes (choice points, tc depth): %i" % len(scopes))
+    print ("last scope:")
+    for s in scopes:
+      s.print()
 
 f = open("WRONG")
-p = paragraph(f.readlines())
-root = parser(p)
-root.print_tree()
+p = ContextParser()
+p.parse(f)
